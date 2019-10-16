c00f5f4021a6d21221d51ca9d359af8a0f0f27c2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 09:39:53 +0200

    Processi

--- ProcessiApp/Program.cs
+++ ProcessiApp/Program.cs
@@ -11,9 +11,44 @@ namespace ProcessiApp
     {
         static void Main(string[] args)
         {
+            // Titolo
             Console.WriteLine("Esercitazione sulla gestione");
-            Process.Start("Notepad.exe");
+
+            // Primo processo,apertura di un file text
+            Process.Start("Notepad.exe", @"\\dc01srv\_Condivisa\INF\4 B\Deboli Christopher\Processo_Gestione\ProcessiApp\File\HelloWorld.txt");
+
+            //Apertura di un link
+            Process.Start("Chrome.exe", "@ https://classroom.google.com/h");
+
+
+            //-----------------------------------------------------------------------------------------------------------------------------------
+
+
+            var app = new Process(); // Aprire un processo con la variabile var
+
+            app.StartInfo.FileName = @"Notepad.exe"; // Apro il programma Notepad.exe
+            
+            app.StartInfo.Arguments = @"\\dc01srv\_Condivisa\INF\4 B\Deboli Christopher\Processo_Gestione\ProcessiApp\"; // Apertura
+            
+            app.Start(); // 
+            
+            app.PriorityClass = ProcessPriorityClass.RealTime; // 
+            
+            //app.WaitForExit(); // Esce solo quando clicco la freccia
+
+            /*var processes = Process.GetProcesses();
+
+            foreach (var p in processes)
+            {
+                if (p.ProcessName == "notepad") // Chiude tutte le app di nome "Notepad"
+                {
+                    p.Kill(); // Chiudo app
+                }
+            }*/
+
+            Console.WriteLine("Processo Terminato");
             Console.ReadLine();
+
         }
     }
 }
